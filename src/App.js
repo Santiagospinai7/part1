deba9b3ee645e9b6372b50a893ab103f72eee9d4
Santiago Ospina Idrobo <santiagospidrobo15@gmail.com>
2023-04-09 23:15:36 +0200

    create different components and use props

--- src/App.js
+++ src/App.js
@@ -1,9 +1,33 @@
 function App() {
+  const friends = [
+    { name: 'Peter', age: 4 },
+    { name: 'Maya', age: 10 },
+  ];
+
+  return (
+    <>
+      <h1>Greetings</h1>
+      <Hello name={friends[0].name} age={friends[0].age} />
+      <Hello name={friends[1].name} age={friends[1].age} />
+      <Footer />
+    </>
+  );
+}
+
+const Hello = (props) => {
   return (
     <div>
-      <p>Hello world</p>
+      <p>Hello {props.name} you're {props.age}</p>
     </div>
-  );
+  )
+}
+
+const Footer = () => {
+  return (
+    <div>
+      Greetings app created by <a href="https://github.com/Santiagospinai7">Santiagospinai7</a>
+    </div>
+  )
 }
 
 export default App;
