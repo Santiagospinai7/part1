5b7ac38bf66e1f24891e2bb7998f151e53976b01
Santiago Ospina Idrobo <santiagospidrobo15@gmail.com>
2023-05-05 14:22:38 +0200

    create line styles

--- src/Components/LoginFrom.js
+++ src/Components/LoginFrom.js
@@ -1,27 +1,45 @@
+import { useState } from 'react';
+
 const LoginForm = ({ handleLoginSubmit, handleChangeUserName}) => {
+  // UseState
+  const [loginVisible, setLoginVisible] = useState(false);
+
   const [username, setUsername, password, setPassword] = handleChangeUserName;
   
+  // Line Styles
+  const hideWhenVisible = { display: loginVisible ? 'none' : '' }
+  const showWhenVisible = { display: loginVisible ? '' : 'none' }
+
   return (
     <div>
-      <h1>Login</h1>
-    
-      <form onSubmit={handleLoginSubmit}>
-        <input
-          type="text"
-          value={username}
-          name="Username"
-          placeholder="Username"
-          onChange={({ target }) => setUsername(target.value)}
-        />
-        <input
-          type="password"
-          value={password}
-          name="Password"
-          placeholder="Password"
-          onChange={({ target }) => setPassword(target.value)}
-        />
-        <button>Login</button>
-      </form>
+
+      <div style={hideWhenVisible}>
+        <button onClick={() => setLoginVisible(true)}>Show Login</button>
+      </div>
+
+      <div style={showWhenVisible}>
+        <h1>Login</h1>
+      
+        <form onSubmit={handleLoginSubmit}>
+          <input
+            type="text"
+            value={username}
+            name="Username"
+            placeholder="Username"
+            onChange={({ target }) => setUsername(target.value)}
+          />
+          <input
+            type="password"
+            value={password}
+            name="Password"
+            placeholder="Password"
+            onChange={({ target }) => setPassword(target.value)}
+          />
+          <button>Login</button>
+        </form>
+
+        <button onClick={() => setLoginVisible(false)}>Cancel</button>
+      </div>
     </div>
   )
 }
